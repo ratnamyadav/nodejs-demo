94df4a500ef9a75f1cb28548f14b5fef8cd9802c
Ratnam Yadav <ratnam.yadav@icloud.com>
2018-11-30 22:12:54 +0530

    api connencted with db for product

--- api/routes/products.js
+++ api/routes/products.js
@@ -1,47 +1,90 @@
 const express = require('express');
-
 const router = express.Router();
+const mongoose = require('mongoose');
+
+const Product = require('../models/product');
 
 router.get('/', (req, res, next) => {
-    res.status(200).json({
-        message: 'Handling GET requests /products'
-    })
+    Product.find()
+        .exec()
+        .then(docs => {
+            res.status(200).json({
+                products: docs
+            })
+        })
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.post('/', (req, res, next) => {
-    const product = {
+    const product = new Product({
+        _id: new mongoose.Types.ObjectId(),
         name: req.body.name,
         price: req.body.price
-    };
-    res.status(201).json({
-        message: 'Handling POST requests /products',
-        createdProduct: product
-    })
+    });
+    product.save().then(result => {
+        res.status(201).json({
+            message: 'Handling POST requests /products',
+            createdProduct: product
+        })
+    }).catch(err => {
+        res.status(500).json({
+            error: err
+        })
+    });
 });
 
 router.get('/:productId', (req, res, next) => {
     const id = req.params.productId;
-    if(id == 'special') {
-        res.status(200).json({
-            message: 'Handling GET requests special /products/:id'
+    Product.findById(id)
+        .exec()
+        .then(doc => {
+            if(doc) {
+                res.status(200).json(doc);
+            } else {
+                res.status(404).json({
+                    message: 'NO valid entry found.'
+                });
+            }
         })
-    } else {
-        res.status(200).json({
-            message: 'Handling GET requests /products/:id'
+        .catch(err => {
+            res.status(500).json({
+                error: err
+            })
         })
-    }
 });
 
 router.patch('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Updated Product'
-    })
+    const id = req.params.productId;
+    Product.update({ _id: id }, { $set: { name: req.body.name, price: req.body.price } })
+        .exec()
+        .then(result => {
+            console.log(result);
+            res.status(200).json(result);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 router.delete('/:productId', (req, res, next) => {
-    res.status(200).json({
-        message: 'Deleted Product'
-    })
+    const id = req.params.productId;
+    Product.remove({ _id: id })
+        .exec()
+        .then(result => {
+            res.status(200).json(result);
+        })
+        .catch( err => {
+            res.status(500).json({
+                error: err
+            })
+        });
 });
 
 module.exports = router;
